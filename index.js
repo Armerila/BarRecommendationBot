44c46fd249c3466d8f772d2dd3fefebcfddfcd96
junskU <tmruuskanen@outlook.com>
2019-12-15 14:23:54 +0200

    Added changes to index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+require('dotenv').config();
+let recommendations = require ('./controllers/recommendations');
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
@@ -7,21 +8,35 @@ console.log(token);
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
+let addBarDialog = {
+  step1: false,
+  step2: false
+};
+let addBarInfo = {};
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  //bot.sendMessage(chatId, 'Received your message');
-
   msgText = msg.text.toString();
-  console.log(msgText);
-  switch (msgText) {
-    case "/getRecommendation":
-      bot.sendMessage(chatId, 'I would recommend the following bars:');
-      console.log("msg from ID: " + msg.from.id);
-      break;
-    default:
-      bot.sendMessage(chatId, 'Invalid command!');
-      break;
+
+  if (msg.chat.type == "private") {
+    if (addBarDialog.step1) {
+      if (addBarDialog.step2) {
+        addBarInfo.address = msgText;
+        bot.sendMessage(chatId, 'A new bar added to the database!');
+        recommendations.insertBar(addBarInfo);
+        addBarDialog.step1 = false;
+        addBarDialog.step2 = false;
+      } else {
+        addBarInfo.name = msgText;
+        bot.sendMessage(chatId, 'Thank you, now give an address for the bar:');
+        addBarDialog.step2 = true;
+      }
+    } else {
+      if (msgText == "/addBar") {
+        bot.sendMessage(chatId, 'Please give a name for the bar:');
+        addBarDialog.step1 = true;
+        addBarInfo = {};
+      }
+    }
   }
 });
