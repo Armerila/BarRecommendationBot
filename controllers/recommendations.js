c1647483a2934c0c1a74b568b0780da98162cc8f
junskU <tmruuskanen@outlook.com>
2019-12-16 06:58:18 +0200

    Changed things

--- controllers/recommendations.js
+++ controllers/recommendations.js
@@ -55,8 +55,9 @@ module.exports = {
   },
 
   getSingleRecommendation: function(userRatings, callback) {
-    //console.log(userRatings);
+    console.log(userRatings);
     recommender.getTopCFRecommendations(userRatings, 0, {limit: 3}, (recommendations) => {
+      console.log(recommendations);
       callback(recommendations);
     });
   }
